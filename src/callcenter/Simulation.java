470bda1f2e9228491615844208f53544c7867c7c
Moritz Gehlhaar <moritz.gehlhaar@aucos.de>
2020-05-04 13:29:31 +0200

    Adjusted to lab version

--- src/callcenter/Simulation.java
+++ src/callcenter/Simulation.java
@@ -15,36 +15,39 @@ public class Simulation {
     public Sink sink;
     public Machine mach;
 
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        double[] arrivalTimes = new double[]{0.4, 1.2, 0.5, 1.7, 0.2, 1.6, 0.2, 1.4, 1.9};
-        double[] serviceTimes = new double[]{2.0, 0.7, 0.2, 1.1, 3.7, 0.6, 4.0, 4.0, 4.0};
 
+
+    public static void main(String[] args) {
+        double[] arrivalTimes = new double[]{0.4, 1.2, 0.5, 1.7, 0.2, 1.6, 0.2, 1.4, 1.9, 1000};
+        double[] serviceTimes = new double[]{2.0, 0.7, 0.2, 1.1, 3.7, 0.6, 4.0, 4.0, 4.0};
 
         // Create an eventlist
         CEventList l = new CEventList();
+
+        /*for (int i = 0; i < arrivalTimes.length; i++) {
+            l.add();
+        })*/
+
         // A queue for the machine
         Queue q = new Queue();
 
-	/*
-	// A source
-    Source s = new Source(q,l,"Source 1");
-
-    // A machine
-    Machine m = new Machine(q,si,l,"Machine 1");
-	*/
+        // A source
+        // Source s = new Source(q, l, "Source 1");
+        Source s = new Source(q, l, "Source 1", arrivalTimes);
 
         // A sink
         Sink si = new Sink("Sink 1");
 
-
-        for (int i = 0; i < arrivalTimes.length; i++) {
-            l.add(m, 0, arrivalTimes[i]);
-        }
+        // A machine
+        // Machine m = new Machine(q,si,l,"Machine 1");
+        Machine m = new Machine(q, si, l, "Machine 1", serviceTimes);
 
         // start the eventlist
         l.start(2000); // 2000 is maximum time
     }
+
 }
