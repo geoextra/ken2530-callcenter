7f14279dd16442a335bc9f4749d27ccb964c1142
Moritz Gehlhaar <moritz.gehlhaar@aucos.de>
2020-05-04 14:04:07 +0200

    Remove fixed times

--- src/callcenter/Simulation.java
+++ src/callcenter/Simulation.java
@@ -22,8 +22,8 @@ public class Simulation {
 
 
     public static void main(String[] args) {
-        double[] arrivalTimes = new double[]{0.4, 1.2, 0.5, 1.7, 0.2, 1.6, 0.2, 1.4, 1.9, 1000};
-        double[] serviceTimes = new double[]{2.0, 0.7, 0.2, 1.1, 3.7, 0.6, 4.0, 4.0, 4.0};
+        // double[] arrivalTimes = new double[]{0.4, 1.2, 0.5, 1.7, 0.2, 1.6, 0.2, 1.4, 1.9, 1000};
+        // double[] serviceTimes = new double[]{2.0, 0.7, 0.2, 1.1, 3.7, 0.6, 4.0, 4.0, 4.0};
 
         // Create an eventlist
         CEventList l = new CEventList();
@@ -37,14 +37,14 @@ public class Simulation {
 
         // A source
         // Source s = new Source(q, l, "Source 1");
-        Source s = new Source(q, l, "Source 1", arrivalTimes);
+        Source s = new Source(q, l, "Source 1");
 
         // A sink
         Sink si = new Sink("Sink 1");
 
         // A machine
         // Machine m = new Machine(q,si,l,"Machine 1");
-        Machine m = new Machine(q, si, l, "Machine 1", serviceTimes);
+        Machine m = new Machine(q, si, l, "Machine 1");
 
         // start the eventlist
         l.start(2000); // 2000 is maximum time
